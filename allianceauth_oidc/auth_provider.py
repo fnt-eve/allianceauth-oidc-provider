590e500755a1978284f745f2f309a5bb42fe1ae8
greg2010 <greg2008200@gmail.com>
2025-05-04 18:22:41 -0400

    add additional claims to acces token

--- allianceauth_oidc/auth_provider.py
+++ allianceauth_oidc/auth_provider.py
@@ -11,15 +11,20 @@ class AllianceAuthOAuth2Validator(OAuth2Validator):
     def _load_application(self, client_id, request):
         client = super()._load_application(client_id, request)
         return client
-
+    
     def get_additional_claims(self):
-        out = {
-            "name": lambda request: request.user.profile.main_character.character_name,
-            "email": lambda request: request.user.email,
-            "groups": lambda request: list(request.user.groups.all().values_list('name', flat=True)) + [request.user.profile.state.name]
-        }
-        return out
+        return _get_additional_claims()
+
+def _get_additional_claims():
+    out = {
+        "name": lambda request: request.user.profile.main_character.character_name,
+        "email": lambda request: request.user.email,
+        "groups": lambda request: list(request.user.groups.all().values_list('name', flat=True)) + [request.user.profile.state.name]
+    }
+    return out
 
 def token_generator(request):
-    func = tokens.signed_token_generator(os.environ.get("AA_OIDC_RSA_PRIVATE_KEY"), issuer=os.environ.get("AA_OIDC_ISSUER"))
+    claims = _get_additional_claims()(request)
+    claims['issuer'] = os.environ.get("AA_OIDC_ISSUER")
+    func = tokens.signed_token_generator(os.environ.get("AA_OIDC_RSA_PRIVATE_KEY"), **claims)
     return func(request)
